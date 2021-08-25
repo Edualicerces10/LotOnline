306203aa8372a8affe6bf075e0e04da000437922
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-08-25 12:53:03 +0100

    alter card only

--- src/components/ContentTicket/Card.tsx
+++ src/components/ContentTicket/Card.tsx
@@ -37,64 +37,11 @@ import {
                   Luanda
                 </Text>
               </Stack>
-              <Stack spacing={0} align={'center'}>
-                <Text fontSize={['large', 'sm', 'xl']} color={'gray.500'}>
-                  ➡
-                </Text>
-              </Stack>
-              <Stack spacing={0} align={'center'}>
-                <Text>Para</Text>
-                <Text fontWeight={600} fontSize={['large', 'sm', 'xl']} color={'gray.500'}>
-                  Malanje
-                </Text>
-              </Stack>
-            </Stack>
-  
-            <Button
-              w={'full'}
-              as="a"
-              href="https://api.whatsapp.com/send?phone=244931611370&text=Olá%20LotOnline%20Angola"
-              target="_blank"
-              mt={8}
-              bg={useColorModeValue('#151f21', 'gray.900')}
-              color={'white'}
-              rounded={'md'}
-              _hover={{
-                transform: 'translateY(-2px)',
-                boxShadow: 'lg',
-              }}>
-              Reservar
-            </Button>
-          </Box>
-        </Box>
-        <Box
-          maxW={'270px'}
-          w={'full'}
-          bg={useColorModeValue('white', 'gray.800')}
-          boxShadow={'2xl'}
-          rounded={'md'}
-          overflow={'hidden'}>
-          
-          <Box p={6}>
-            <Stack spacing={0} align={'center'} mb={5}>
-              <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
-                15.000kz
-              </Heading>
-              <Text color={'gray.500'}>Viagem de ida e volta</Text>
-            </Stack>
-  
-            <Stack direction={'row'} justify={'center'} spacing={6}>
-              <Stack spacing={0} align={'center'}>
-                <Text>De</Text>
-                <Text fontSize={['large', 'sm', 'xl']} color={'gray.500'}>
-                  Luanda
-                </Text>
-              </Stack>
-              <Stack spacing={0} align={'center'}>
-                <Text fontSize={'3xl'} color={'gray.500'}>
-                  ➡
-                </Text>
-              </Stack>
+                {/* <Stack spacing={0} align={'center'}>
+                  <Text fontSize={['large', 'sm', 'xl']} color={'gray.500'}>
+                    ➡
+                  </Text>
+                </Stack> */}
               <Stack spacing={0} align={'center'}>
                 <Text>Para</Text>
                 <Text fontWeight={600} fontSize={['large', 'sm', 'xl']} color={'gray.500'}>
@@ -143,11 +90,11 @@ import {
                   Luanda
                 </Text>
               </Stack>
-              <Stack spacing={0} align={'center'}>
+              {/* <Stack spacing={0} align={'center'}>
                 <Text fontSize={'3xl'} color={'gray.500'}>
                   ➡
                 </Text>
-              </Stack>
+              </Stack> */}
               <Stack spacing={0} align={'center'}>
                 <Text>Para</Text>
                 <Text fontWeight={600} fontSize={['large', 'sm', 'xl']} color={'gray.500'}>
@@ -196,11 +143,11 @@ import {
                   Luanda
                 </Text>
               </Stack>
-              <Stack spacing={0} align={'center'}>
+              {/* <Stack spacing={0} align={'center'}>
                 <Text fontSize={'3xl'} color={'gray.500'}>
                   ➡
                 </Text>
-              </Stack>
+              </Stack> */}
               <Stack spacing={0} align={'center'}>
                 <Text>Para</Text>
                 <Text fontWeight={600} fontSize={['large', 'sm', 'xl']} color={'gray.500'}>
@@ -249,11 +196,64 @@ import {
                   Luanda
                 </Text>
               </Stack>
-              <Stack spacing={0} align={'center'}>
+              {/* <Stack spacing={0} align={'center'}>
                 <Text fontSize={'3xl'} color={'gray.500'}>
                   ➡
                 </Text>
+              </Stack> */}
+              <Stack spacing={0} align={'center'}>
+                <Text>Para</Text>
+                <Text fontWeight={600} fontSize={['large', 'sm', 'xl']} color={'gray.500'}>
+                  Malanje
+                </Text>
               </Stack>
+            </Stack>
+  
+            <Button
+              w={'full'}
+              as="a"
+              href="https://api.whatsapp.com/send?phone=244931611370&text=Olá%20LotOnline%20Angola"
+              target="_blank"
+              mt={8}
+              bg={useColorModeValue('#151f21', 'gray.900')}
+              color={'white'}
+              rounded={'md'}
+              _hover={{
+                transform: 'translateY(-2px)',
+                boxShadow: 'lg',
+              }}>
+              Reservar
+            </Button>
+          </Box>
+        </Box>
+        <Box
+          maxW={'270px'}
+          w={'full'}
+          bg={useColorModeValue('white', 'gray.800')}
+          boxShadow={'2xl'}
+          rounded={'md'}
+          overflow={'hidden'}>
+          
+          <Box p={6}>
+            <Stack spacing={0} align={'center'} mb={5}>
+              <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
+                15.000kz
+              </Heading>
+              <Text color={'gray.500'}>Viagem de ida e volta</Text>
+            </Stack>
+  
+            <Stack direction={'row'} justify={'center'} spacing={6}>
+              <Stack spacing={0} align={'center'}>
+                <Text>De</Text>
+                <Text fontSize={['large', 'sm', 'xl']} color={'gray.500'}>
+                  Luanda
+                </Text>
+              </Stack>
+              {/* <Stack spacing={0} align={'center'}>
+                <Text fontSize={'3xl'} color={'gray.500'}>
+                  ➡
+                </Text>
+              </Stack> */}
               <Stack spacing={0} align={'center'}>
                 <Text>Para</Text>
                 <Text fontWeight={600} fontSize={['large', 'sm', 'xl']} color={'gray.500'}>
