7f5413516af84d49d31fc8206fb7cbe05b28a917
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-08-25 12:25:25 +0100

    text alterable

--- src/components/ContentTicket/Text.tsx
+++ src/components/ContentTicket/Text.tsx
@@ -2,7 +2,7 @@ import { Text, Container} from '@chakra-ui/react'
 export default function TextDestino(){
   return (
     <Container maxW="container.xl" my="10" borderLeft="2px" borderColor="orange.700">
-      <Text fontSize={['6rem', '1.2rem']}>
+      <Text fontSize={[ '1.2rem','1.4rem']}>
         The quick brown fox jumps over the lazy dog is an English-language pangram—a
         sentence that contains all of the letters of the English alphabet. Owing to
         its existence, Chakra was created.
