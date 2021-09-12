b08e6140036e655dbdb2295a7a2969e9deebd57f
Edualicerces10 <eduardoalicerces96@hotmail.com>
2021-09-12 22:19:46 +0100

    add a description in website

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,12 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+                    <link rel="shortcut icon" href="/img/icon-512.png" />
+                    <meta name="theme-color" content="#FF7900" />
+                    <meta
+                    name="description"
+                    content="Somos é uma empresa angolana que tem como objectivo encotrar passageiros na internet que pretendem viajar no interior de Angola e a nossa missão é atender com respeito, responsablidade e qualidade os nossos clientes."
+                    />
                 </Head>
             
                 <body>
